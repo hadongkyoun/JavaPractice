9265d05cc19e26328dc290ed908c9f002dc7776c
devKyoun <72578757+hadongkyoun@users.noreply.github.com>
2023-05-08 00:44:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 672 ms, Memory: 59388 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,39 +4,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
+	
+	/*
+		br.readLine() 같이 파일처리 등 입출력 관련된 코드를 사용할땐
+	 	IOException 예외처리를 해줘야 한다.
+	*/
+	 public static  void main(String[] args) throws IOException{
 
-	public static void main(String args[]) throws IOException{
+		//M이 100,000개 하면 그 안에서 System.out.println을 100,000개 불러야 하기 때문에
+		StringBuilder sb = new StringBuilder();
+
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		//수의 갯수
+		int N = Integer.parseInt(st.nextToken());
+		//합을 구해야 하는 횟수
+		int M = Integer.parseInt(st.nextToken());
 
-		StringBuilder sb = new StringBuilder();
-		
-		
-		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(stringTokenizer.nextToken());
-		int M = Integer.parseInt(stringTokenizer.nextToken());
-		
-		int[] sums = new int[N];
-	
-		stringTokenizer = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++){
-			if(i == 0)
-				sums[0] = Integer.parseInt(stringTokenizer.nextToken());
-			else
-				sums[i] = sums[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+		/*
+			배열입력
+			int[] arr = new int[N];
+		*/
+
+		//구간 합 배열 (인덱스 1부터 시작)
+		int[] S = new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i <= N; i++){
+			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
 		}
 
+		//구간 합 출력
+		int left_th, right_th;
+		for(int i=0; i < M; i++){
+			st = new StringTokenizer(br.readLine());
+			left_th = Integer.parseInt(st.nextToken());
+			right_th = Integer.parseInt(st.nextToken());
 
-		for(int i=0; i<M; i++){
-			stringTokenizer = new StringTokenizer(br.readLine());
-			int left = Integer.parseInt(stringTokenizer.nextToken());
-			int right = Integer.parseInt(stringTokenizer.nextToken());
-			if(left == 1)
-				sb.append(sums[right-1] + "\n");
-			else
-				sb.append(sums[right-1] - sums[left-2] + "\n");
+			sb.append(S[right_th] - S[left_th-1] + "\n");
+
 		}
-
+		
 		System.out.println(sb);
 	}
 }
